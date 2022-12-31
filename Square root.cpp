c646a2e1a69df1d8bd47bdbcf1f40a83bfab62f1
UroojIslamKhan <121480546+Urooj-Islam@users.noreply.github.com>
2022-12-31 11:54:03 +0500

    Add files via upload

---
+++ Square root.cpp
@@ -0,0 +1,13 @@
+//To Find Squareroot of any number
+#include<iostream>
+#include<cmath>    //header file for sqrt(
+using namespace std;
+int main()
+{
+float num;
+cout<<"Enter any number";
+cin>>num;
+cout<<"Squre root of "<<num<<" is: "<<sqrt(num);
+return 0;
+
+}
