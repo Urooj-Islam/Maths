cca987c900525b534f02cd79f2e79d5db7851410
UroojIslamKhan <121480546+Urooj-Islam@users.noreply.github.com>
2023-01-03 17:02:05 +0500

    Add files via upload

---
+++ Maximum Number.cpp
@@ -0,0 +1,13 @@
+//Tp find Larger number
+#include<iostream>
+using namespace std;
+int main(){
+	
+float a, b;
+
+cout<<"Enter two numbers";
+cin>>a>>b;
+cout<<"The larger number is: "<<max(a,b);
+	
+	return 0;
+}
