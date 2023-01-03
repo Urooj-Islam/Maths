e2b88180523ad7597ee54e7770fb8a6c8dd1dc37
UroojIslamKhan <121480546+Urooj-Islam@users.noreply.github.com>
2023-01-03 17:01:38 +0500

    Add files via upload

---
+++ Ceiling and FLoor.cpp
@@ -0,0 +1,13 @@
+//Find roundedd up qnd rouned down
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	float num;
+	cout<<"Enter any number ";
+	cin>>num;
+	cout<<"Rouded up of "<<num<<" is: "<<ceil(num);
+	cout<<"\n Rounded down of "<<num<<" is:"<<floor(num);
+	return 0;
+}
